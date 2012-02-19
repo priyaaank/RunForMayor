5d937dacd82af453c1afcaf8458811314d18d464
Priyank Gupta <priyaaank@gmail.com>
2012-02-19 20:28:17 +0530

    Testing my RACK ENV on heroku

--- webhook.rb
+++ webhook.rb
@@ -1,5 +1,11 @@
+require 'httparty'
+require 'ostruct'
+require 'yaml'
+require 'mongoid'
 require 'sinatra'
+Dir.foreach(".") {|file| require_relative file if (file.include?(".rb") and file != 'run.rb') }
 
 get '/hi' do
-  "Dragons be here!"
+  # "Dragons be here!"
+  ENV["RACK_ENV"]
 end
