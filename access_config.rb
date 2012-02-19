b71fd285d4d0198cd4886c270b0af61aa12be18d
Priyank Gupta <priyaaank@gmail.com>
2012-02-19 15:42:42 +0530

    Moving 4sq config to config folder

--- access_config.rb
+++ access_config.rb
@@ -1,7 +1,7 @@
 class AccessConfig
   
   def initialize
-    @config = @config = YAML::load(File.open("./4sq_config.yml"))
+    @config = @config = YAML::load(File.open("./config/4sq_config.yml"))
   end
 
   def access_token
